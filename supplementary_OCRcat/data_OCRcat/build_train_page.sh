c7c055f3544661df74a0c16b727af531b971f244
gabays <gabay.simon@gmail.com>
2021-03-08 13:03:45 +0100

    additional data

---
+++ supplementary_OCRcat/data_OCRcat/build_train_page.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Delete dir if it exists
+rm -r trainingData; mkdir trainingData
+#if [ -d trainingData ]; then rm -Rf trainingData; fi
+mkdir -p trainingData
+#getting images
+cp */png/*png trainingData
+cp */jpg/*jpg trainingData
+#getting the page files
+cp */page/*xml trainingData
